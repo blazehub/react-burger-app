1ec705490c4f5e0ea3aa9dc248c4e88099ae5825
Avinash Thakur <blaze.avinash@gmail.com>
2020-06-08 07:09:09 +0530

    logo component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,9 +3,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 const layout = (props) => (
     <Aux>
         <div>
+            <Toolbar />
             {/* Toolbar,SideDrawer,Backdrop */}
         </div>
         <main className={classes.Content}>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+
+import classes from './Toolbar.css';
+
+const Toolbar = (props) => {
+    return (
+        <header className={classes.Toolbar}>
+            <div>MENU</div>
+            <Logo />
+            <nav>
+                ...
+           </nav>
+        </header>
+    )
+}
+
+export default Toolbar;
