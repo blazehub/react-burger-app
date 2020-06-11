4887071ad3a0f58c1dd9ab37bc8c1b087ef3edf3
Avinash Thakur <blaze.avinash@gmail.com>
2020-06-11 20:53:32 +0530

    project restructure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import Button from '../../UI/Button/Button';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 
-const OrderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{props.ingredients[igKey]}</li>
-        });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A Delicious burger with following ingredient:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseConitnued}>CONTINUE</Button>
-        </Aux>
-    )
+class OrderSummary extends PureComponent {
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{this.props.ingredients[igKey]}</li>
+            });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A Delicious burger with following ingredient:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseConitnued}>CONTINUE</Button>
+            </Aux>
+        )
+    }
 }
 
 export default OrderSummary;
